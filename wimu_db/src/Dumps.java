2542b48daf5e9bdab891419ba907098c21869f74
Andre Valdestilhas <firmao@gmail.com>
2017-08-08 22:59:33 +0200

    Update Dumps.java

--- wimu_db/src/Dumps.java
+++ wimu_db/src/Dumps.java
@@ -1,55 +1,64 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 import java.util.Scanner;
 
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.io.FileUtils;
 
 public class Dumps {
 
 	public static void main(String args[]) throws IOException {
-		readBz2File();
+		String url = "http://downloads.dbpedia.org/2016-10/core-i18n/en/labels_en.ttl.bz2";
+		readBz2File(url);
+		// readBz2FileParallel(url);
 	}
 
-	public static void readBz2File() throws IOException {
-		URL url = new URL("http://downloads.dbpedia.org/2016-10/core-i18n/en/labels_en.ttl.bz2");
+	public static void readBz2File(String pURL) throws IOException {
+		URL url = new URL(pURL);
 		Scanner scanner = new Scanner(
 				new BufferedReader(new InputStreamReader(new BZip2CompressorInputStream(url.openStream()))));
 		String triple;
 		try {
+
+			int lines = 0;
+			// IntStream.range(0,lines).parallel().forEach(action);
+
 			while ((triple = scanner.nextLine()) != null) {
 				// Treat your triples here :)
-				System.out.println(triple);
+				String[] spo = triple.split("> ");
+				if (spo.length > 2) {
+					String obj = spo[2];
+					
+					if(!obj.startsWith("<htt")){
+						System.out.println("Object is Literal !!!");
+						System.out.println(triple);
+						System.out.println("Object: " + obj);
+					}
+				}
 			}
 		} finally {
 			scanner.close();
 		}
 	}
 
-	public static File downloadFile(URL url, String path) throws IOException {
-		File ret = new File(path);
-		System.out.println("opening connection");
-		InputStream in = url.openStream();
-		FileOutputStream fos = new FileOutputStream(ret);
+	public static void readBz2FileParallel(String pURL) throws IOException {
+		URL url = new URL(pURL);
+		try {
 
-		System.out.println("reading file...");
-		int length = -1;
-		byte[] buffer = new byte[1024];// buffer for portion of data from
-		// connection
-		while ((length = in.read(buffer)) > -1) {
-			fos.write(buffer, 0, length);
+			// File f = FileUtils.toFile(url);
+			File f1 = new File("/home/andre/nada.bz2");
+			FileUtils.copyURLToFile(url, f1);
+			List<String> lst = FileUtils.readLines(f1, "UTF-8");
+
+			lst.parallelStream().forEach(line -> {
+				System.out.println(line);
+			});
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
-
-		fos.close();
-		in.close();
-		System.out.println("file was downloaded from:");
-		System.out.println(url.toString());
-		return ret;
 	}
-
 }
