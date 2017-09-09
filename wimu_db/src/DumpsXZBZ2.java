743f22d4f18c1a9908d0aba0b3f5d28fdc61e734
Andre Valdestilhas <firmao@gmail.com>
2017-09-09 12:54:06 +0200

    Update DumpsXZBZ2.java

--- wimu_db/src/DumpsXZBZ2.java
+++ wimu_db/src/DumpsXZBZ2.java
@@ -6,11 +6,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.RandomAccessFile;
 import java.net.SocketException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -21,6 +24,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.jena.atlas.lib.Tuple;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.system.StreamRDF;
 import org.jsoup.Jsoup;
@@ -32,60 +36,78 @@ import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.sparql.core.Quad;
 
-public class DumpsXZBZ2 {
+public class Dumps {
 
 	public static Set<String> alreadyProcessed = new HashSet<String>();
 	public static Set<String> errorFiles = new HashSet<String>();
+	public static Set<String> successFiles = new HashSet<String>();
+	public static Set<String> setFileURLs = new HashSet<String>();
+	public static Map<String, String> datasetErrorsJena = new HashMap<String, String>();
+	public static int count = 0;
 
 	public static void main(String args[]) throws IOException {
 		long start = System.currentTimeMillis();
 
 		int cores = Runtime.getRuntime().availableProcessors();
 		List<String> lstURLDumps = FileUtils.readLines(new File("dumpsLocation.txt"), "UTF-8");
-		Set<String> setAllFileServer = getFileNames(lstURLDumps);
-
+		Set<String> setAllFileURLs = getFileURLs(lstURLDumps);
+		setFileURLs.addAll(setAllFileURLs);
 		// setAllFileServer.forEach(fileName -> {
-		for (String fileName : setAllFileServer) {
-			String justFName = getURLFileName(fileName);
-			if (alreadyProcessed.contains(justFName))
+		for (String fileURL : setAllFileURLs) {
+			if (alreadyProcessed.contains(fileURL))
 				continue;
 
 			/*
 			 * get(download) only files that are not processed yet according to
 			 * the number of cores.
 			 */
-			Set<File> setFiles = null;
+			Set<FileWIMU> setFiles = null;
 			try {
-				setFiles = getFiles(cores, setAllFileServer);
+				setFileURLs.removeAll(alreadyProcessed);
+				
+				System.out.println("Starting downloading " + cores + " Files. Already processed files: " 
+				+ alreadyProcessed.size() + " from " + setAllFileURLs.size());
+				setFiles = getFiles(cores, setFileURLs);
 			} catch (IOException e) {
-				String s[] = fileName.split("/");
-				String sFileName = s[s.length - 1];
-				errorFiles.add(sFileName);
+				errorFiles.add(fileURL);
+				e.printStackTrace();
 			}
 
 			// One file for each processor thread.
+			System.out.println("Starting to process " + cores + " threads/files (PARALLEL). Already processed files: " 
+				+ alreadyProcessed.size() + " from " + setAllFileURLs.size());
 			setFiles.parallelStream().forEach(file -> {
-				if (processCompressedFile(file, fileName)) {
-					alreadyProcessed.add(file.getName());
+			//for (FileWIMU file : setFiles) {
+				String provenance = file.getDataset();
+				if (processCompressedFile(file, provenance)) {
+					successFiles.add(provenance);
+					System.out.println("SUCESS: " + provenance);
 				} else {
-					errorFiles.add(file.getName());
+					System.out.println("FAIL: " + provenance + " ERROR: " + datasetErrorsJena.get(provenance));
 				}
+				alreadyProcessed.add(provenance);
+				// setAllFileURLs.removeAll(alreadyProcessed);
+			//}
 			});
-			// });
 		}
 		long totalTime = System.currentTimeMillis() - start;
 		System.out.println("Total Time(ms): " + totalTime);
+		String errors = "Files processed: " + setAllFileURLs.size() + "\n Total Time: " + totalTime + ""
+				+ "\nNumberErrorFiles: " + datasetErrorsJena.size() + "\nNumberFilesSuccess: " + successFiles.size();
+		Email.sendEmail("firmao@gmail.com", "End pro DBindex process", errors);
+		generateFile(datasetErrorsJena, "ErrorsJena.csv");
 	}
 
-	private static boolean processCompressedFile(File file, String dataset) {
+	private static boolean processCompressedFile(FileWIMU file, String dataset) {
 		boolean ret = false;
 		try {
-			File fUnzip = null;
+			FileWIMU fUnzip = null;
 			if (file.getName().endsWith(".bz2"))
-				fUnzip = new File(file.getName().replaceAll(".bz2", ""));
+				fUnzip = new FileWIMU(file.getName().replaceAll(".bz2", ""));
 			else
-				fUnzip = new File(file.getName().replaceAll(".xz", ""));
+				fUnzip = new FileWIMU(file.getName().replaceAll(".xz", ""));
 
+			fUnzip.setDataset(file.getDataset());
 			BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
 			FileOutputStream out = new FileOutputStream(fUnzip);
 
@@ -112,7 +134,7 @@ public class DumpsXZBZ2 {
 			file.delete();
 
 			if (fUnzip != null)
-				ret = processUnzipRDF(fUnzip, dataset);
+				ret = processUnzipRDF(fUnzip);
 		} catch (Exception ex) {
 			ret = false;
 			ex.printStackTrace();
@@ -120,7 +142,7 @@ public class DumpsXZBZ2 {
 		return ret;
 	}
 
-	private static boolean processUnzipRDF(File fUnzip, String dataset) {
+	private static boolean processUnzipRDF(FileWIMU fUnzip) {
 		boolean ret = true;
 		try {
 			StreamRDF reader = new StreamRDF() {
@@ -165,12 +187,10 @@ public class DumpsXZBZ2 {
 						// + "> " + triple.getObject().toString() + " .");
 
 						try {
-							DBUtil.insert(triple.getSubject().getURI(), dataset);
+							DBUtil.insert(triple.getSubject().getURI(), fUnzip.getDataset());
 						} catch (ClassNotFoundException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} catch (SQLException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					}
@@ -183,40 +203,48 @@ public class DumpsXZBZ2 {
 				}
 
 			};
-			RDFDataMgr.parse(reader, fUnzip.getName());
+			if (fUnzip.getName().endsWith(".tql")) {
+				// removeFirstLine(fUnzip.getName());
+				// removeLastLine(fUnzip.getName());
+				RDFDataMgr.parse(reader, fUnzip.getName(), Lang.NQUADS);
+			} else if (fUnzip.getName().endsWith(".ttl")) {
+				RDFDataMgr.parse(reader, fUnzip.getName(), Lang.TTL);
+			} else {
+				RDFDataMgr.parse(reader, fUnzip.getName());
+			}
 			fUnzip.delete();
 		} catch (Exception ex) {
+			datasetErrorsJena.put(fUnzip.getDataset(), ex.getMessage());
 			ret = false;
 		}
 		return ret;
 	}
 
-	private static Set<File> getFiles(int cores, Set<String> setAllFileServer) throws IOException {
-		Set<File> ret = new HashSet<File>();
-		int count = 0;
+	private static Set<FileWIMU> getFiles(int cores, Set<String> setFileServer) throws IOException {
+		Set<FileWIMU> ret = new HashSet<FileWIMU>();
+		count = 0;
 		// setAllFileNames.removeAll(alreadyProcessed);
-		for (String sFile : setAllFileServer) {
-			String sFileName = getURLFileName(sFile);
-			if (alreadyProcessed.contains(sFileName))
-				continue;
-			if (count > cores)
-				break;
-
-			File fRet = new File(sFileName);
-			URL url = new URL(sFile);
-			FileUtils.copyURLToFile(url, fRet);
-
-			ret.add(fRet);
-			count++;
-		}
-
+		setFileServer.parallelStream().limit(cores).forEach(sFileURL -> {
+			// for (String sFile : setAllFileServer) {
+			try {
+				String sFileName = getURLFileName(sFileURL);
+				FileWIMU fRet = new FileWIMU(sFileName);
+				URL url = new URL(sFileURL);
+				FileUtils.copyURLToFile(url, fRet);
+				fRet.setDataset(sFileURL);
+				ret.add(fRet);
+				count++;
+				System.out.println(count + " : " + sFileURL);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			// }
+		});
 		return ret;
 	}
 
-	private static Set<String> getFileNames(List<String> pURLs) throws SocketException, IOException {
+	private static Set<String> getFileURLs(List<String> pURLs) throws SocketException, IOException {
 		Set<String> ret = new HashSet<String>();
-
-		
 
 		pURLs.forEach(sURL -> {
 			if (sURL.startsWith("ftp")) {
@@ -231,22 +259,24 @@ public class DumpsXZBZ2 {
 					client.login("anonymous", "");
 					FTPFile[] files = client.listFiles(path);
 					for (FTPFile ftpFile : files) {
-						//ret.add("ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/rdf/" + ftpFile.getName());
+						// ret.add("ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/rdf/"
+						// + ftpFile.getName());
 						String fName = ftpFile.getName();
-						if(fName.endsWith(".xz"))
+						if (fName.endsWith(".xz"))
 							ret.add(sURL + fName);
 					}
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			} else {
-				//String url = "http://downloads.dbpedia.org/2016-10/core-i18n/en/";
+				// String url =
+				// "http://downloads.dbpedia.org/2016-10/core-i18n/en/";
 				try {
 					Document doc = Jsoup.connect(sURL).get();
 					for (Element file : doc.select("a")) {
 						String fName = file.attr("href");
 						if (fName.endsWith(".bz2"))
-							ret.add(fName);
+							ret.add(sURL + fName);
 					}
 				} catch (Exception ex) {
 					ex.printStackTrace();
@@ -401,5 +431,54 @@ public class DumpsXZBZ2 {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+
+	public static void removeFirstLine(String fileName) throws IOException {
+		RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
+		// Initial write position
+		long writePosition = raf.getFilePointer();
+		raf.readLine();
+		// Shift the next lines upwards.
+		long readPosition = raf.getFilePointer();
+
+		byte[] buff = new byte[1024];
+		int n;
+		while (-1 != (n = raf.read(buff))) {
+			raf.seek(writePosition);
+			raf.write(buff, 0, n);
+			readPosition += n;
+			writePosition += n;
+			raf.seek(readPosition);
+		}
+		raf.setLength(writePosition);
+		raf.close();
+	}
+
+	public static void removeLastLine(String fileName) throws IOException {
+		RandomAccessFile f = new RandomAccessFile(fileName, "rw");
+		long length = f.length() - 1;
+		byte b;
+		do {
+			length -= 1;
+			f.seek(length);
+			b = f.readByte();
+		} while (b != 10);
+		f.setLength(length + 1);
+		f.close();
+	}
+
+	public static File generateFile(Map<String, String> endPointErrors, String fileName) {
+		File ret = new File(fileName);
+		try {
+			PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+			endPointErrors.forEach((endPoint, error) -> {
+				writer.println(endPoint + "\t" + error);
+			});
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ret;
 	}
 }
