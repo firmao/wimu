68eea914918a53391b53815daf168e69b290e5ff
Andre Valdestilhas <firmao@gmail.com>
2017-10-12 18:33:52 +0200

    including searchTables SP

--- DB_Tables_StoredProcedure.sql
+++ DB_Tables_StoredProcedure.sql
@@ -66,3 +66,45 @@ End if;
 COMMIT;
 END
 //
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SearchTables`(in _uri varchar(6000))
+BEGIN
+	DECLARE v_finished INTEGER DEFAULT 0;
+	DECLARE v_table varchar(600) DEFAULT "";
+    DECLARE v_n varchar(600) DEFAULT "";
+    
+    DEClARE uri_cursor CURSOR FOR 
+		SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES  WHERE table_schema = 'linklion2' and TABLE_NAME like 'datatypes_%';
+
+	DECLARE CONTINUE HANDLER 
+        FOR NOT FOUND SET v_finished = 1;
+    
+    drop temporary table if exists tmp;
+    
+    create temporary table tmp (
+	dtypes int,
+	dataset varchar(6000) DEFAULT NULL
+	) engine=memory;
+    
+	OPEN uri_cursor;
+	get_uri: LOOP	 
+		FETCH uri_cursor INTO v_table;
+
+		IF v_finished = 1 THEN 
+			LEAVE get_uri;
+		END IF;
+		
+        
+        SET @sql_text1 = concat('Insert into tmp(dtypes, dataset) Select count(o) as dType, o from ',v_table
+        , ' where s=\'',_uri,'\' group by o');
+
+        PREPARE stmt1 FROM @sql_text1;
+		EXECUTE stmt1;
+		DEALLOCATE PREPARE stmt1;
+		 
+	 END LOOP get_uri;
+
+	CLOSE uri_cursor;
+	
+    Select sum(dtypes) as dataTypes, dataset from tmp group by dataset;
+END
