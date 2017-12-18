d7ce0d93dd7f2aa398130c87d44baf1ad8ae3fa4
Andre Valdestilhas <firmao@gmail.com>
2017-12-18 13:29:19 +0100

    Add files via upload

---
+++ wimu_ws/src/servlets/UploadHDTfile.java
@@ -0,0 +1,84 @@
+package servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+ * Servlet implementation class UploadHDTfile
+ */
+public class UploadHDTfile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	public UploadHDTfile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response)
+             throws ServletException, IOException {
+         boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+         if (isMultipart) {
+             // Create a factory for disk-based file items
+             FileItemFactory factory = new DiskFileItemFactory();
+
+             // Create a new file upload handler
+             ServletFileUpload upload = new ServletFileUpload(factory);
+
+             try {
+                 // Parse the request
+                 List items = upload.parseRequest(request);
+                 Iterator iterator = items.iterator();
+                 while (iterator.hasNext()) {
+                     FileItem item = (FileItem) iterator.next();
+                     if (!item.isFormField()) {
+                         String fileName = item.getName();    
+                         String root = getServletContext().getRealPath("/");
+                         File path = new File(root + "/uploads");
+                         if (!path.exists()) {
+                             boolean status = path.mkdirs();
+                         }
+
+                         File uploadedFile = new File(path + "/" + fileName);
+                         System.out.println(uploadedFile.getAbsolutePath());
+                         item.write(uploadedFile);
+                         response.getOutputStream().println(uploadedFile.getAbsolutePath());
+                     }
+                 }
+             } catch (FileUploadException e) {
+                 e.printStackTrace();
+             } catch (Exception e) {
+                 e.printStackTrace();
+             }
+         }
+     }
+
+
+}
