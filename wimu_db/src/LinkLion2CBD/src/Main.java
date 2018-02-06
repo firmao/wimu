e7fe15f464563dc36610bf252be2350eeee5e7fe
Andre Valdestilhas <firmao@gmail.com>
2018-02-06 15:01:58 +0100

    Add files via upload

--- wimu_db/src/LinkLion2CBD/src/Main.java
+++ wimu_db/src/LinkLion2CBD/src/Main.java
@@ -12,6 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.jena.atlas.logging.LogCtl;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -20,6 +21,8 @@ import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.impl.ModelCom;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.rdfhdt.hdt.hdt.HDT;
 import org.rdfhdt.hdt.hdt.HDTManager;
 import org.rdfhdt.hdtjena.HDTGraph;
@@ -28,41 +31,160 @@ import com.google.gson.Gson;
 
 public class Main {
 
+	static final String WIMUservice = "http://dmoussallem.ddns.net:1550/LinkLion2_WServ/Find?link=";
+	// static final String WIMUservice =
+	// "http://139.18.8.58:8080/LinkLion2_WServ/Find?link=";
+	static final String linksetsRepo = "http://www.linklion.org/download/mapping/";
+	static final boolean onlyTest = false;
+
 	public static void main(String[] args) throws Exception {
-		Set<WIMUri> ret = readJson();
-		System.out.println("ret.size: " + ret.size());
-		PrintWriter writerS = new PrintWriter("cbdS.nt", "UTF-8");
-		PrintWriter writerT = new PrintWriter("cbdT.nt", "UTF-8");
-		for (WIMUri wUri : ret) {
-			//System.out.println(wUri);
-			Map<Set<String>, Set<String>> mCBD_s_t = getAllCBD(wUri);
-			mCBD_s_t.entrySet().forEach(entry ->{
-				Set<String> cbdS = entry.getKey();
-				Set<String> cbdT = entry.getValue();
-				cbdS.forEach(tripleS ->{
-					writerS.println(tripleS);
-				});
-				cbdT.forEach(tripleT ->{
-					writerT.println(tripleT);
-				});
-			});
-		}
-		writerS.close();
-		writerT.close();
+		// process();
+		processParallel();
 	}
 
-	private static Map<Set<String>, Set<String>> getAllCBD(WIMUri wUri) throws Exception {
+	public static void process() throws Exception {
+		LogCtl.setLog4j("log4j.properties");
+		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
+
+		long start = System.currentTimeMillis();
+		// String linkset =
+		// "http://139.18.8.58:8080/LinkLion2_WServ/Find?link=http://www.linklion.org/download/mapping/sws.geonames.org---purl.org.nt";
+
+		Set<String> linksets = getLinksets(linksetsRepo,onlyTest);
+		PrintWriter writerS = new PrintWriter("cbdS.nt", "UTF-8");
+		PrintWriter writerT = new PrintWriter("cbdT.nt", "UTF-8");
+
+		Set<WIMUri> wimuURIs = new HashSet<WIMUri>();
+		// linksets.parallelStream().forEach(linkset -> {
+		linksets.forEach(linkset -> {
+			try {
+				wimuURIs.addAll(readJson(linkset));
+				System.out.println("SUCCESS: " + linkset);
+			} catch (Exception e) {
+				System.out.println("FAIL: " + linkset + " ERROR: " + e.getMessage());
+			}
+		});
+		// wimuURIs.parallelStream().forEach(wUri -> {
+		wimuURIs.forEach(wUri -> {
+			System.out.println(wUri);
+			try {
+				Map<Set<String>, Set<String>> mCBD_s_t = getAllCBD(wUri);
+				// mCBD_s_t.entrySet().forEach(entry -> {
+				mCBD_s_t.entrySet().parallelStream().forEach(entry -> {
+					Set<String> cbdS = entry.getKey();
+					Set<String> cbdT = entry.getValue();
+					if (cbdS != null) {
+						cbdS.parallelStream().forEach(tripleS -> {
+							writerS.println(tripleS);
+						});
+					}
+					if (cbdT != null) {
+						cbdT.parallelStream().forEach(tripleT -> {
+							writerT.println(tripleT);
+						});
+					}
+				});
+			} catch (Exception ex) {
+				System.out.println(wUri + " Error: " + ex.getMessage());
+			}
+		});
+		writerS.close();
+		writerT.close();
+		long totalTime = System.currentTimeMillis() - start;
+		System.out.println("TotalTime: " + totalTime);
+	}
+
+	public static void processParallel() throws Exception {
+		LogCtl.setLog4j("log4j.properties");
+		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
+		System.out.println("Parallel version");
+
+		long start = System.currentTimeMillis();
+		// String linkset =
+		// "http://139.18.8.58:8080/LinkLion2_WServ/Find?link=http://www.linklion.org/download/mapping/sws.geonames.org---purl.org.nt";
+		Set<String> linksets = getLinksets(linksetsRepo,onlyTest);
+		PrintWriter writerS = new PrintWriter("cbdS.nt", "UTF-8");
+		PrintWriter writerT = new PrintWriter("cbdT.nt", "UTF-8");
+
+		Set<WIMUri> wimuURIs = new HashSet<WIMUri>();
+		System.out.println("Number of linksets: " + linksets.size());
+		linksets.parallelStream().forEach(linkset -> {
+			// linksets.forEach(linkset -> {
+			try {
+				wimuURIs.addAll(readJson(linkset));
+				System.out.println("SUCCESS_1: " + linkset);
+			} catch (Exception e) {
+				System.out.println("FAIL: " + linkset + " ERROR: " + e.getMessage());
+			}
+		});
+		System.out.println("Number of URIs to process: " + (wimuURIs.size() * 2));
+		wimuURIs.parallelStream().forEach(wUri -> {
+			// wimuURIs.forEach(wUri -> {
+			System.out.println(wUri);
+			try {
+				Map<Set<String>, Set<String>> mCBD_s_t = getAllCBD(wUri);
+				// mCBD_s_t.entrySet().forEach(entry -> {
+				mCBD_s_t.entrySet().parallelStream().forEach(entry -> {
+					Set<String> cbdS = entry.getKey();
+					Set<String> cbdT = entry.getValue();
+					if (cbdS != null) {
+						cbdS.parallelStream().forEach(tripleS -> {
+							writerS.println(tripleS);
+						});
+					}
+					if (cbdT != null) {
+						cbdT.parallelStream().forEach(tripleT -> {
+							writerT.println(tripleT);
+						});
+					}
+				});
+			} catch (Exception ex) {
+				System.out.println(wUri + " Error: " + ex.getMessage());
+			}
+		});
+		writerS.close();
+		writerT.close();
+		long totalTime = System.currentTimeMillis() - start;
+		System.out.println("TotalTime: " + totalTime);
+	}
+
+	private static Set<String> getLinksets(String urlRepository, boolean bSample) {
+		Set<String> linksets = new HashSet<String>();
+		if (bSample) {
+			linksets.add(WIMUservice + urlRepository + "sws.geonames.org---purl.org.nt");
+			linksets.add(WIMUservice + urlRepository + "citeseer.rkbexplorer.com---nsf.rkbexplorer.com.nt");
+			linksets.add(WIMUservice + urlRepository + "AGROVOC.nt---agclass.nal.usda.gov.nt");
+		} else {
+			try {
+				org.jsoup.nodes.Document doc = Jsoup.connect(urlRepository).get();
+				for (Element file : doc.select("a")) {
+					String fName = file.attr("href");
+					if (fName.endsWith(".nt"))
+						linksets.add(WIMUservice + urlRepository + fName);
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return linksets;
+	}
+
+	private static synchronized Map<Set<String>, Set<String>> getAllCBD(WIMUri wUri) throws Exception {
 		Map<Set<String>, Set<String>> ret = new HashMap<Set<String>, Set<String>>();
-		Set<String> cbdS = getCBD(wUri.getUriS(),wUri.getDatasetS(),wUri.getHdtS());
-		Set<String> cbdT = getCBD(wUri.getUriT(),wUri.getDatasetT(),wUri.getHdtT());
+		Set<String> cbdS = getCBD(wUri.getUriS(), wUri.getDatasetS(), wUri.getHdtS());
+		Set<String> cbdT = getCBD(wUri.getUriT(), wUri.getDatasetT(), wUri.getHdtT());
 		ret.put(cbdS, cbdT);
 		return ret;
 	}
 
-	private static Set<String> getCBD(String uri, String urlDataset, String urlHDT) throws Exception {
-		Set<String> cbd = getCBDHDT(uri,urlHDT);
-		if(cbd.size() < 1){
-			cbd = getCBDDataset(uri,urlDataset);
+	private static synchronized Set<String> getCBD(String uri, String urlDataset, String urlHDT) throws Exception {
+		Set<String> cbd = null;
+		if (urlHDT != null) {
+			cbd = getCBDHDT(uri, urlHDT);
+		}
+		if ((cbd != null) && (cbd.size() < 1) && (urlDataset != null) && (urlDataset.length() > 1)) {
+			System.out.println("NON HDT: " + urlDataset);
+			// cbd = getCBDDataset(uri,urlDataset);
 		}
 		return cbd;
 	}
@@ -82,7 +204,7 @@ public class Main {
 			hdt = HDTManager.mapHDT(file.getAbsolutePath(), null);
 			HDTGraph graph = new HDTGraph(hdt);
 			Model model = new ModelCom(graph);
-			String sparql = "Select * where {?s ?p ?o . filter(?s=<"+uri+"> || ?o=<"+uri+">) }";
+			String sparql = "Select * where {?s ?p ?o . filter(?s=<" + uri + "> || ?o=<" + uri + ">) }";
 
 			Query query = QueryFactory.create(sparql);
 
@@ -91,33 +213,36 @@ public class Main {
 
 			while (results.hasNext()) {
 				QuerySolution thisRow = results.next();
-				//uriDataset = thisRow.get("s").toString() + "\t" + file.getDataset();
+				// uriDataset = thisRow.get("s").toString() + "\t" +
+				// file.getDataset();
 				String nTriple = "<" + thisRow.get("s").toString() + "> <" + thisRow.get("p").toString() + ">";
-				if(thisRow.get("o").isLiteral()){
-					nTriple += thisRow.get("o").asLiteral().toString() + ". ";
-				}else{
+				if (thisRow.get("o").isLiteral()) {
+					nTriple += " \"" + thisRow.get("o").asLiteral().toString() + "\"^^<"
+							+ thisRow.get("o").asLiteral().getDatatypeURI() + "> .";
+				} else {
 					nTriple += " <" + thisRow.get("o").toString() + "> .";
 				}
 				ret.add(nTriple);
 			}
 			qe.close();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}finally {
+		} catch (Exception e) {
+			System.out.println("FAIL: " + urlHDT + " Error: " + e.getMessage());
+		} finally {
 			file.delete();
-			if(hdt != null){
+			if (hdt != null) {
 				hdt.close();
 			}
-		}	
-			
+		}
+
 		return ret;
 	}
 
-	private static  Set<WIMUri> readJson() throws IOException {
-		URL url = new URL("http://139.18.8.58:8080/LinkLion2_WServ/Find?link=http://www.linklion.org/download/mapping/sws.geonames.org---purl.org.nt");
-        InputStreamReader reader = new InputStreamReader(url.openStream());
-        WIMUri[] dto = new Gson().fromJson(reader, WIMUri[].class);
-        Set<WIMUri> ret = Arrays.stream(dto).collect(Collectors.toCollection(HashSet::new));
-        return ret;
+	private static Set<WIMUri> readJson(String linksetURL) throws Exception {
+		System.out.println("Processing linkset with WIMU: " + linksetURL);
+		URL url = new URL(linksetURL);
+		InputStreamReader reader = new InputStreamReader(url.openStream());
+		WIMUri[] dto = new Gson().fromJson(reader, WIMUri[].class);
+		Set<WIMUri> ret = Arrays.stream(dto).collect(Collectors.toCollection(HashSet::new));
+		return ret;
 	}
 }
